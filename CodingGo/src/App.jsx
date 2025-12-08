e9872bb7723f22f0e5215ff985b6096ad4792098
joohyunjin <s25052@gsm.hs.kr>
2025-12-08 17:33:16 +0900

    Register.module.css 파일에서 아무 기능도 하지 않거나 오류가 발생하는 css를 일부 수정하였으며 Register.jsx 파일의 하단에 있는 문구를 수정하였습니다.

--- CodingGo/src/App.jsx
+++ CodingGo/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-// import Login from './components/login' // 임시 코드
+//import Login from './components/Login' // 임시 코드
 import Register from './components/Register' // 임시 코드
 
 function App() {
