e846566e501407128e85eb2732c310406b4fb2a0
joohyunjin <s25052@gsm.hs.kr>
2025-12-18 11:56:38 +0900

    NavBar 탭 UI 제작

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,16 @@
 import styles from './NavBar.module.css'
 import { NavLink } from 'react-router-dom'
 import Logo from '../assets/CodingGo-logo.png'
-import { PiFire } from "react-icons/pi";
 import { RiHomeFill } from "react-icons/ri";
 import { RiHomeLine } from "react-icons/ri";
+import { BsPeople } from "react-icons/bs";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { IoCodeSharp } from "react-icons/io5";
+import { IoCodeSlash } from "react-icons/io5";
+import { BiTrophy } from "react-icons/bi";
+import { BiSolidTrophy } from "react-icons/bi";
+import { RiUser3Line } from "react-icons/ri";
+import { RiUser3Fill } from "react-icons/ri";
 
 function NavBar() {
 
@@ -32,13 +39,99 @@ function NavBar() {
         </div> */}
 
         <div className={styles.NavTab}>
-          <NavLink className={styles.TabItems}
+          <NavLink 
             to='/main'
-            style={({isActive}) => ({
-              backgroundColor: isActive ? 'yellow' : 'transparent'
-            })}>
-            <RiHomeFill size={25} className={styles.TabIcons}/>
-            <p>홈</p>
+            className={({ isActive }) => 
+              isActive ? `${styles.TabItems} ${styles.Active}` : styles.TabItems
+            }>
+              
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <RiHomeFill size={25} className={styles.TabIcons} />
+                ) : (
+                  <RiHomeLine size={25} className={styles.TabIcons} />
+                )}
+                <p>홈</p>
+              </>
+            )}
+
+          </NavLink>
+
+          <NavLink 
+            to='/community'
+            className={({ isActive }) => 
+              isActive ? `${styles.TabItems} ${styles.Active}` : styles.TabItems
+            }>
+              
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <BsFillPeopleFill size={25} className={styles.TabIcons} />
+                ) : (
+                  <BsPeople size={25} className={styles.TabIcons} />
+                )}
+                <p>커뮤니티</p>
+              </>
+            )}
+            
+          </NavLink>
+
+          <NavLink 
+            to='/problems'
+            className={({ isActive }) => 
+              isActive ? `${styles.TabItems} ${styles.Active}` : styles.TabItems
+            }>
+              
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <IoCodeSlash size={25} className={styles.TabIcons} />
+                ) : (
+                  <IoCodeSharp size={25} className={styles.TabIcons} />
+                )}
+                <p>문제</p>
+              </>
+            )}
+            
+          </NavLink>
+
+          <NavLink 
+            to='/ranking'
+            className={({ isActive }) => 
+              isActive ? `${styles.TabItems} ${styles.Active}` : styles.TabItems
+            }>
+              
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <BiSolidTrophy size={25} className={styles.TabIcons} />
+                ) : (
+                  <BiTrophy size={25} className={styles.TabIcons} />
+                )}
+                <p>랭킹</p>
+              </>
+            )}
+            
+          </NavLink>
+
+          <NavLink 
+            to='/mypage'
+            className={({ isActive }) => 
+              isActive ? `${styles.TabItems} ${styles.Active}` : styles.TabItems
+            }>
+              
+            {({ isActive }) => (
+              <>
+                {isActive ? (
+                  <RiUser3Fill size={25} className={styles.TabIcons} />
+                ) : (
+                  <RiUser3Line size={25} className={styles.TabIcons} />
+                )}
+                <p>마이페이지</p>
+              </>
+            )}
+            
           </NavLink>
         </div>
 
