f575c5807b4720fedc94b7c3d937a5c30dd7dde6
joohyunjin <s25052@gsm.hs.kr>
2025-12-22 09:06:24 +0900

    홈 탭 제작 완료

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
         <Route path='/ranking' element={<Ranking />} />
         <Route path='/MyPage' element={<MyPage />} />
       </Route>
+      <Route path='*' element={<h1>페이지를 찾을 수 없습니다 404</h1>} />
     </Routes>
   )
 }
